19c56d05f23ab2720012ef97680364bd6753ae88
Norie06 <modla.nori@gmail.com>
2025-03-07 14:11:45 +0100

    Routing set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
+import Quizzes from './pages/Quizzes';
+import ResourceHub from './pages/ResourceHub';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/community">Community</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/quizzes">Quizzes</Link>
+              </li>
+              <li>
+                <Link to="/resource-hub">Resource Hub</Link>
+              </li>
+              <li>
+                <Link to="/sign-up">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/sign-in">Sign In</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/quizzes" element={<Quizzes />} />
+          <Route path="/resource-hub" element={<ResourceHub />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/pages/ResourceHub.jsx
@@ -0,0 +1,9 @@
+function ResearchHub() {
+  return (
+    <div>
+      <h1>Research Hub</h1>
+    </div>
+  );
+}
+
+export default ResearchHub;
